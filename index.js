ba6bfce5703741c5ba1e85d726d44edfd3b960ff
Shivam-2309 <kapoorshivam7178@gmail.com>
2026-02-03 02:19:49 +0530

    Feat: Basic express app with mongoose setup done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+env.config();
+const app = express(); // express app object
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/home', (req, res) => {
+    console.log("Hitting home");
+    return res.json({
+        success : true,
+        message: "Checkup for my server waking up"
+    });
+});
+
+app.listen(process.env.PORT, async () => {
+    // this is executed once we successfully start the server on the port
+    console.log(`Server started on Port ${process.env.PORT}`);
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Successfully connected to MongoDB");
+    }
+    catch(err){
+        console.log("Problem occured ", err);
+    }
+});
